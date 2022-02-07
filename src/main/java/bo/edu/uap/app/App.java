c2b6f6d95841f8ea5811a281246875636f53c225
Marco Quenta Velasco <marcokani@gmail.com>
2022-02-07 18:57:26 -0400

    first commit

---
+++ src/main/java/bo/edu/uap/app/App.java
@@ -0,0 +1,13 @@
+package bo.edu.uap.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
