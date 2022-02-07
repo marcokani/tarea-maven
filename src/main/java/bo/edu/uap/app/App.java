2861cc64077750c329cfd51c4c68199d27c38a9b
Marco Quenta Velasco <marcokani@gmail.com>
2022-02-07 19:58:02 -0400

    end point tarea con el mensaje desde un recurso

--- src/main/java/bo/edu/uap/app/App.java
+++ src/main/java/bo/edu/uap/app/App.java
@@ -1,4 +1,11 @@
 package bo.edu.uap.app;
+import static spark.Spark.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Hello world!
@@ -6,8 +13,13 @@ package bo.edu.uap.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FileNotFoundException, IOException
     {
-        System.out.println( "Hello World!" );
+        Properties prop=new Properties();
+        prop.load(new FileInputStream(new File("src/main/resources/vars.properties")));
+        // System.out.println( "Hello World!" );
+         System.out.println( "valor recuperado: "+prop.getProperty("PROPIEDAD_1"));
+        String resource= prop.getProperty("PROPIEDAD_1");
+        get("/tarea", (req, res) -> "En el ambiente '"+resource+"'");
     }
 }
